7d0bbe46051aa820712e18758a81f9c9cea19d88
Okaki030 <h.okawa0303@gmail.com>
2022-05-28 12:29:02 +0900

    ecrの操作権限を持つassume roleを作成

--- cicd/main/iam.tf
+++ cicd/main/iam.tf
@@ -5,3 +5,34 @@ resource "aws_iam_user" "github" {
       Name = "${local.system_name}-github"
   }
 }
+
+resource "aws_iam_role" "deployer" {
+  name = "${local.system_name}-deployer"
+
+  assume_role_policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "sts:AssumeRole",
+            "sts:TagSession"
+          ],
+          "Principal" : {
+            "AWS" : aws_iam_user.github.arn # example-prod-foobar-github のIAMユーザーからAssumeRoleが許可
+          }
+        }
+      ]
+    }
+  )
+
+  tags = {
+    Name = "${local.system_name}-deployer"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "role_deployer_ecr_power_user" {
+  role       = aws_iam_role.deployer.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
+}
