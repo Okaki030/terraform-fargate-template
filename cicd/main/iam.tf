e51b27eab7ee3be327985055b2308f50d9a787d5
Okaki030 <h.okawa0303@gmail.com>
2022-05-29 13:06:39 +0900

    ecspresso用のdataを定義

--- cicd/main/iam.tf
+++ cicd/main/iam.tf
@@ -1,8 +1,8 @@
 resource "aws_iam_user" "github" {
-  name   = "${local.system_name}-github"
-  
+  name = "${local.system_name}-github"
+
   tags = {
-      Name = "${local.system_name}-github"
+    Name = "${local.system_name}-github"
   }
 }
 
